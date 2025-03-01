41781e3dda2232c57b54ca57d5830dc59ea80877
jehan <jehancjacobs@gmail.com>
2025-03-01 14:22:26 -0800

    a

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import numpy as n
+import numpy as np
 import sqlite3
-# from datetime import datetime
-
 from flask import Flask, render_template, request, jsonify, send_from_directory, url_for
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask_wtf import FlaskForm
@@ -14,8 +12,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sadlfsdbkg'
 app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
 
-
-## Initialize the database we'll use to store the name:quant:exp of food
+# Initialize the database we'll use to store the name:quantity:expiration date of food
 def initialize_database():
     connector = sqlite3.connect('thefridge.db')
     c = connector.cursor()
@@ -26,13 +23,12 @@ def initialize_database():
 
 initialize_database()
 
-
-## Route to the homepage
+# Route to the homepage
 @app.route("/")
 def home():
     return render_template(' ')
 
-
+## Create a FlaskForm composed for uploading
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 
@@ -45,13 +41,13 @@ class UploadForm(FlaskForm):
     )
     submit = SubmitField('Upload')
 
+# Route to retrieve uploaded files by filename
 @app.route("/uploads/<filename>")
 def getfilename(filename):
     return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
 
-
+# Route to upload images and store extracted information in the database
 @app.route("/upload_items", methods=['POST', 'GET'])
-
 def upload_image():
     form = UploadForm()
     if form.validate_on_submit():
@@ -60,39 +56,36 @@ def upload_image():
         image_components = make_ai_call(file_url).split(':')
         connector = sqlite3.connect('thefridge.db') 
         c = connector.cursor()
-        ## Convert List into record for Sql data base ??? 
+        # Convert List into record for Sql data base ??? 
         c.execute("")
     else:
         file_url = None
-    
-    ## Doesnt do anythin
     return
 
-
+# Route to add food items to the database
 # @app.route("/add_items", methods=['POST'])
 def item_add():
     data = request.json
-    name= data['name']
+    name = data['name']
     quantity = int(data['quantity'])
     expiration = data['expiration']
 
     connector = sqlite3.connect('thefridge.db')
     c = connector.cursor()
-    c.execute("INSERT INTO food (namne, quantity, expiration) VALUES (?, ?, ?)", (name, quantity, expiration))
+    c.execute("INSERT INTO food (name, quantity, expiration) VALUES (?, ?, ?)", (name, quantity, expiration))
     connector.commit()
     connector.close()
 
+# Route to retrieve all food items from the database and return them as JSON
 # @app.route('/get_items', methods=['GET'])
 def get_items():
     connector = sqlite3.connect("thefridge.db")
     c = connector.cursor()
-    c.execute("SELECT * from items")
+    c.execute("SELECT * from food")
     items = c.fetchall()
     connector.close()
 
     return jsonify({"items": items})
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
